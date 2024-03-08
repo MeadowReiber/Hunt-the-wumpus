b650dce608e5bd52decc11e973426b9d798eda96
helenxiaa <2014479@apps.nsd.org>
2024-03-08 14:23:23 -0800

    helen commit

---
+++ GameControl/GameControl.java
@@ -0,0 +1,16 @@
+package GameControl;
+public class GameControl{
+    public static void main(String[] args){
+        System.out.println("this is my file");
+    }
+    public Boolean moveForward(int roomNumber){
+        //called by ui --> cave checks for open rooms --> return if can move
+        return true;
+    }
+    public void buyArrow(){
+        //called by ui --> trivia --> player to update inventory
+    }
+    
+
+
+}
