1cfff2452e1cee12cf82ac3c49a02807a4b9e383
casandrarey <1121294@apps.nsd.org>
2024-03-08 13:51:47 -0800

    stubbed version

--- GraphicalInterface.java
+++ GraphicalInterface.java
@@ -1,4 +1,34 @@
-// Casandra Reyes
+//Casandra Reyes
 public class GraphicalInterface {
+    ////////////
+    //Properties
+    ////////////
 
+    ////////////
+    //Constructor
+    ////////////
+    public GraphicalInterface(){
+        
+    }
+
+    ////////////
+    //Methods
+    ////////////
+    public void displayRoom(){
+        //displays the hexagonal room in the form of 6 buttons with exits
+    }
+    public int displayScore(){
+        //called by game control?
+        return 8; //not sure if it is an int or double
+    }
+    public void displayInventory(){
+        //calls player
+        //returns an array?
+    }
+    public String displayHints(){
+        return "I smell a wumpus";
+    }
+    public void displayActions(){
+        //calls player?
+    }
 }
