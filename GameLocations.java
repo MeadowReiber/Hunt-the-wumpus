e8b6f2b7a81ccd02944c731530d36bb8fbee8a6a
MeadowReiber <1108718@apps.nsd.org>
2024-03-08 14:27:27 -0800

    startStub

--- GameLocations.java
+++ GameLocations.java
@@ -1,9 +1,25 @@
 //Meadow Reiber
 public class GameLocations{
     //fields and properties----------------------
+    private int batCave;
+    private int pitCave;
+    private int wumpusCave;
+
+    private int playerPos;
+
     //constructor--------------------------------
     public GameLocations(){
-
+        this.startGame();
     }
     //methods------------------------------------
+    public void startGame(){
+        this.batCave = 2;
+    }
+    public void moveHazard(){
+
+    }
+    public void movePlayer(){}
+    public String giveHint(){
+        return "hint";
+    }
 }
