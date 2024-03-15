aefd4ed0c738c0e87cb8f9076d127f9a8ac074f6
MeadowReiber <70457730+MeadowReiber@users.noreply.github.com>
2024-03-15 07:07:59 +0000

    update player class

--- Player.java
+++ Player.java
@@ -1,17 +1,31 @@
 //Meadow Reiber
 public class Player {
-    //fields and properties----------------------
-    private int arrowInventory;
+  //fields and properties----------------------
+  private int arrows;
+  private int goldCoins;
 
-    //constructor--------------------------------
-    public Player(){
+  private int turns;
+
+  //constructor--------------------------------
+  public Player(){
       
+  }
+  //methods------------------------------------
+  public void takeTurn(){
+    this.turns++;
+  }
+  public int getArrowInvetory(){
+    return this.arrows;
+  }
+  public void shootArrow(){
+    this.arrows -=;
+  }
+  public int calculateScore(boolean wumpusDead){
+    int score = 100 - this.turns + this.goldCoins + (5 * this.arrows);
+    if(wumpusDead){
+      score += 50;
+      return score;
     }
-    //methods------------------------------------
-    public int getArrowInvetory(){
-      return this.arrowInventory;
-    }
-    public void shootArrow(){
-      this.arrowInventory -=;
-    }
+    return score;
+  }
 }
