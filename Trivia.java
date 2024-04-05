f3ab92c2245ec5cd57dd835b2369152713920fca
Thelittlebox06 <2022546@apps.nsd.org>
2024-04-05 14:25:28 -0700

    commit

--- Trivia.java
+++ Trivia.java
@@ -2,56 +2,32 @@
 //crystal B 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-
-public class TriviaQuestion {
-
-    String answer;
-    String Question;
-  
-
-    
-}
+import java.util.ArrayList;
 
 public class Trivia{
   String hint;
-    
+  ArrayList<TriviaQuestion> TriviaQuestions;
 
-    public Trivia(){
-        String filePath;
-       
-         
-        Trivia[] readQuestionsFromFile(String triviaquestions) throws FileNotFoundException{
-          Trivia[] qq = new Trivia[3];
-          int count =0;
-          File file = new File();
-          Scanner scanner = new Scanner(file);
+  public Trivia() throws FileNotFoundException{
+      String filePath = "triviaquestions.txt";      
+      Trivia[] qq = new Trivia[3];
+      int count =0;
+      File file = new File(filePath);
+      Scanner scanner = new Scanner(file);
 
-          While(scanner.hasNextLine()){
-            String question = scanner.nextLine();
-          }
-        }
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-        String line;
-        while ((line = reader.readLine()) != null) {
-          // do something with line .. split 
-         // Custom input string
-        String str = "triviaquestions.txt";
-        String[] Questions = str.split("triviaquestions.txt", 6);
-
-        }
-        //how to make the file doc, into somthing i can use a mthod on .split
-        System.out.println("split successful.");
-        return;
-
-      } catch (IOException e) {
-        System.out.println("Error loading file: " + e.getMessage());
+      while(scanner.hasNextLine()){
+        String line = scanner.nextLine();            
+        String[] questionSource = line.split(",", 6);
+        TriviaQuestion question = new TriviaQuestion(questionSource[0],Arrays.copyOfRange(questionSource, 1, 4) );
+        TriviaQuestions.add(question);
       }
-    }
+  }
 
     public TriviaQuestion GetQuestion(){
 
---
+++ TriviaQuestion.java
@@ -0,0 +1,9 @@
+public class TriviaQuestion {
+     String[] Answers;
+    String Question;
+
+    public TriviaQuestion(String question,String[] answers){
+      Question = question;
+      Answers = answers;
+    }
+}
