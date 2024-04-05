d6700faf8f0ec11cebd8c70b4ac442068e7f729d
Thelittlebox06 <2022546@apps.nsd.org>
2024-04-05 13:55:50 -0700

    commit

--- Trivia.java
+++ Trivia.java
@@ -2,25 +2,39 @@
 //crystal B 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 public class TriviaQuestion {
 
     String answer;
     String Question;
-    
+  
 
     
 }
 
 public class Trivia{
-
+  String hint;
     
 
     public Trivia(){
         String filePath;
+       
+         
+        Trivia[] readQuestionsFromFile(String triviaquestions) throws FileNotFoundException{
+          Trivia[] qq = new Trivia[3];
+          int count =0;
+          File file = new File();
+          Scanner scanner = new Scanner(file);
+
+          While(scanner.hasNextLine()){
+            String question = scanner.nextLine();
+          }
+        }
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
         String line;
         while ((line = reader.readLine()) != null) {
@@ -49,8 +63,9 @@ public class Trivia{
 
 
     }
+   
     public String Hints(){
-
+      System.out.println("hint:"+ hint);
     // will return hints
     //reutrn boolean, if answer if right/wrong
     }
