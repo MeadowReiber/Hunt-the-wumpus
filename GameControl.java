716238e2b306743c565aef8cfbcaefaed9598359
helenxiaa <2014479@apps.nsd.org>
2024-05-22 12:46:23 -0700

    weew

--- GameControl.java
+++ GameControl.java
@@ -8,21 +8,30 @@ public class GameControl{
     Player player = new Player();
     Cave cave = new Cave();
     GameLocations loc = new GameLocations(player, cave);
+    GUI display = new GUI("game");
     public int displayScore(int room){
         //ask meadow which class holds the boolean for wumpus dead or alive?
         int currentScore = player.calculateScore(shoot(room));
         return currentScore;
         //called by ui --> player to get score --> ui
     }
-    public Boolean shoot(int room){
+    public void shoot(int room){
+        Boolean shot;
         if(player.getArrows() > 0){
             //checks if there r arrows
-            return loc.shootArrow(room);
+            shot = loc.shootArrow(room);
             //shootarrow will return true for shot, false for not shot
         }
         else{
-            return false;
+            shot =  false;
             //if no arrows, then did not shoot
+        }
+
+        if(shot == true){
+            //call casandra's method
+        }
+        else{
+            //call casasndra's method
         }
     }
     public Boolean moveForward(int roomNumber){
@@ -30,13 +39,18 @@ public class GameControl{
         return true;
     }
     public void buyArrow(){
+        int currentCoins = player.getCoins();
+        int currentArrows = player.getArrows();
+        if (currentCoins > 0){
+            currentCoins--;
+            currentArrows++;
+            //call casandras method to display coins
+            //call casandras method to display arrows
+        }
         //called by ui --> trivia --> player to update inventory
     }
-    public int[] displayInventory(){
-        int arrows = player.getArrows();
-        int coins = player.getCoins();
-        int[] inventory = {arrows, coins};
-        return inventory;
+    public void displayInventory(){
+        GUI.displayInventory(player);
         //called by ui --> player to get inventory --> ui
     }
     public void displayActions(){
