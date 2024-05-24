5a6dfb604e2ec0592ada6d01b8c6349203ec16c2
MeadowReiber <1108718@apps.nsd.org>
2024-05-24 14:03:44 -0700

    try

--- GameLocations.java
+++ GameLocations.java
@@ -97,9 +97,13 @@ public class GameLocations{
 
     public String giveHint(){
       int hint = (int)(Math.random()*10);
+      if(hint < 3){
+        return hints[hint];
+      }
+
       //random number to determine hints
       //  
-      return "hint";
+      return "" + ;
     }
     public ArrayList<String> giveWarnings(){
       ArrayList<Integer> rooms = map.getAdjacentRooms(this.playerPos);
