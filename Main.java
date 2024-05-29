e003539a009a55a71241d3dd4b32726e0c0baef9
1108888 <112448698+1108888@users.noreply.github.com>
2024-05-29 13:45:07 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,14 @@ import javax.swing.JFrame;
 
 public class Main {
     public static void main(String[] args) {
-        GUI theGUI = new GUI("wumpus");
+        JFrame frame = new JFrame("Hunt the Wumpus");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1000, 600);
+
+        StartPanel startPanel = new StartPanel(frame);
+        frame.add(startPanel);
+        frame.setVisible(true);
+
+        startPanel.requestFocusInWindow();
     }
 }
