9d9bfc31eaf59de199789b41c03ac30a41713981
Thelittlebox06 <2022546@apps.nsd.org>
2024-05-29 12:15:44 -0700

    hi

--- Trivia.java
+++ Trivia.java
@@ -36,26 +36,29 @@ public class Trivia{
     public TriviaQuestion GetQuestion(){
     // return a random question from the file
     //create a list of questions
+    //cannot repeat questions until all have been given/when game resets
+    
     Random random = new Random();
     int RandomIndex = random.nextInt(TriviaQuestions.size());
     TriviaQuestion ranQuestions = TriviaQuestions.get(RandomIndex);
 
     System.out.println("Question:" + ranQuestions.Question);
     System.out.println("choose the correct answer");
-
+    
+    
+      //Scanner
 
     for(int i = 0; i  < ranQuestions.Answers.length; i++){
-      System.out.println((i + 1) + "." + ranQuestions.Answers[i]);
+      System.out.println((i + 1) + ". " + ranQuestions.Answers[i]);
     }
     return ranQuestions;
      
-// you will get a question from the tivaquestion
     }
-    public String getAnswers(TriviaQuestion theanswers){
+    public String getAnswer(TriviaQuestion theanswers){
+
 
       return theanswers.Answers[0];
     }
-
 
 
 }
